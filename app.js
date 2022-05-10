d2ff8134c69f1a1ad2429f530c9c04f95180f75e
TJSeabury <tylerseabury@gmail.com>
2022-05-10 14:37:47 -0400

    oops

--- app.js
+++ app.js
@@ -28,88 +28,90 @@ const urlResolver = ( uri, hostname = null ) => {
   }
 };
 
-const app = express();
-const port = 3000;
+( async () => {
+  const app = express();
+  const port = 3000;
 
-// support parsing of application/json type post data
-//app.use( bodyParser.json() );
-app.use( bodyParser.text() );
+  // support parsing of application/json type post data
+  //app.use( bodyParser.json() );
+  app.use( bodyParser.text() );
 
-//support parsing of application/x-www-form-urlencoded post data
-app.use( bodyParser.urlencoded( { extended: true } ) );
+  //support parsing of application/x-www-form-urlencoded post data
+  app.use( bodyParser.urlencoded( { extended: true } ) );
 
-const here = [];
+  const here = [];
 
-here.push( 'init browser' );
-const browser = await puppeteer.launch( {
-  args: ['--no-sandbox', '--disable-setuid-sandbox'],
-} );
+  here.push( 'init browser' );
+  const browser = await puppeteer.launch( {
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  } );
 
 
-app.get( '/', async ( req, res ) => {
-  try {
-    const url = req.query?.url;
-    here.push( 'url: ' + url );
-    const [target, urlResolverErr] = urlResolver( url );
-    here.push( 'target: ' + target + ' ; urlResolverErr' + urlResolverErr );
-    if ( !target || urlResolverErr ) {
-      return res.status( 422 ).json( 'Must provide a valid URL.' );
-    }
-
+  app.get( '/', async ( req, res ) => {
     try {
-      here.push( 'new page' );
-      const page = await browser.newPage();
-      here.push( 'navigate to page' );
-      await page.goto( target, { waitUntil: 'networkidle0' } );
-      here.push( 'get content' );
-      const html = await page.content();
-      here.push( 'close page' );
-      page.close();
+      const url = req.query?.url;
+      here.push( 'url: ' + url );
+      const [target, urlResolverErr] = urlResolver( url );
+      here.push( 'target: ' + target + ' ; urlResolverErr' + urlResolverErr );
+      if ( !target || urlResolverErr ) {
+        return res.status( 422 ).json( 'Must provide a valid URL.' );
+      }
+
+      try {
+        here.push( 'new page' );
+        const page = await browser.newPage();
+        here.push( 'navigate to page' );
+        await page.goto( target, { waitUntil: 'networkidle0' } );
+        here.push( 'get content' );
+        const html = await page.content();
+        here.push( 'close page' );
+        page.close();
+      } catch ( err ) {
+        here.push( browser );
+      }
+
+      const virtualConsole = new VirtualConsole();
+      virtualConsole.on( "error", () => { } ); // No-op to skip console errors.
+
+      const dom = new JSDOM( html, {
+        virtualConsole
+      } );
+      here.push( 'start jsdom' );
+      if ( !dom ) {
+        return res.status( 500 ).json( 'Failed to create vDOM. :c' );
+      }
+
+      here.push( 'strip scripts' );
+      const scripts = Array.from( dom.window.document.querySelectorAll( 'script' ) );
+      for ( const script of scripts ) {
+        if ( script.parentNode ) {
+          script.parentNode.removeChild( script );
+        }
+      }
+
+      here.push( 'strip styles' );
+      const styles = Array.from( dom.window.document.querySelectorAll( 'link[rel="stylesheet"],style' ) );
+      for ( const style of styles ) {
+        if ( style.parentNode ) {
+          style.parentNode.removeChild( style );
+        }
+      }
+
+      here.push( 'extract modded body' );
+      const body = dom.window.document.querySelector( 'body' );
+
+      here.push( 'return' );
+      return res.status( 200 ).json( body?.innerHTML || '' );
     } catch ( err ) {
-      here.push( browser );
+      return res.status( 500 ).json( {
+        error: err,
+        message: "There has apperently been an error.",
+        here
+      } );
     }
+  } );
 
-    const virtualConsole = new VirtualConsole();
-    virtualConsole.on( "error", () => { } ); // No-op to skip console errors.
-
-    const dom = new JSDOM( html, {
-      virtualConsole
-    } );
-    here.push( 'start jsdom' );
-    if ( !dom ) {
-      return res.status( 500 ).json( 'Failed to create vDOM. :c' );
-    }
-
-    here.push( 'strip scripts' );
-    const scripts = Array.from( dom.window.document.querySelectorAll( 'script' ) );
-    for ( const script of scripts ) {
-      if ( script.parentNode ) {
-        script.parentNode.removeChild( script );
-      }
-    }
-
-    here.push( 'strip styles' );
-    const styles = Array.from( dom.window.document.querySelectorAll( 'link[rel="stylesheet"],style' ) );
-    for ( const style of styles ) {
-      if ( style.parentNode ) {
-        style.parentNode.removeChild( style );
-      }
-    }
-
-    here.push( 'extract modded body' );
-    const body = dom.window.document.querySelector( 'body' );
-
-    here.push( 'return' );
-    return res.status( 200 ).json( body?.innerHTML || '' );
-  } catch ( err ) {
-    return res.status( 500 ).json( {
-      error: err,
-      message: "There has apperently been an error.",
-      here
-    } );
-  }
-} );
-
-app.listen( port, () => {
-  console.log( `Example app listening on port ${port}` );
-} );
+  app.listen( port, () => {
+    console.log( `Example app listening on port ${port}` );
+  } );
+} )();
