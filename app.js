03cff05dc8dd4408dba977254a70632a30583f83
TJSeabury <tylerseabury@gmail.com>
2022-05-10 13:11:57 -0400

    try catch

--- app.js
+++ app.js
@@ -39,45 +39,49 @@ app.use( bodyParser.text() );
 app.use( bodyParser.urlencoded( { extended: true } ) );
 
 app.get( '/', async ( req, res ) => {
-  const url = req.query?.url;
-  const [target, urlResolverErr] = urlResolver( url );
-  if ( !target || urlResolverErr ) {
-    return res.status( 422 ).json( 'Must provide a valid URL.' );
-  }
-
-  const browser = await Puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto( target, { waitUntil: 'networkidle0' } );
-  const html = await page.content();
-  await browser.close();
-
-  const virtualConsole = new VirtualConsole();
-  virtualConsole.on( "error", () => { } ); // No-op to skip console errors.
-
-  const dom = new JSDOM( html, {
-    virtualConsole
-  } );
-  if ( !dom ) {
-    return res.status( 500 ).json( 'Failed to create vDOM. :c' );
-  }
-
-  const scripts = Array.from( dom.window.document.querySelectorAll( 'script' ) );
-  for ( const script of scripts ) {
-    if ( script.parentNode ) {
-      script.parentNode.removeChild( script );
+  try {
+    const url = req.query?.url;
+    const [target, urlResolverErr] = urlResolver( url );
+    if ( !target || urlResolverErr ) {
+      return res.status( 422 ).json( 'Must provide a valid URL.' );
     }
-  }
 
-  const styles = Array.from( dom.window.document.querySelectorAll( 'link[rel="stylesheet"],style' ) );
-  for ( const style of styles ) {
-    if ( style.parentNode ) {
-      style.parentNode.removeChild( style );
+    const browser = await Puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto( target, { waitUntil: 'networkidle0' } );
+    const html = await page.content();
+    await browser.close();
+
+    const virtualConsole = new VirtualConsole();
+    virtualConsole.on( "error", () => { } ); // No-op to skip console errors.
+
+    const dom = new JSDOM( html, {
+      virtualConsole
+    } );
+    if ( !dom ) {
+      return res.status( 500 ).json( 'Failed to create vDOM. :c' );
     }
+
+    const scripts = Array.from( dom.window.document.querySelectorAll( 'script' ) );
+    for ( const script of scripts ) {
+      if ( script.parentNode ) {
+        script.parentNode.removeChild( script );
+      }
+    }
+
+    const styles = Array.from( dom.window.document.querySelectorAll( 'link[rel="stylesheet"],style' ) );
+    for ( const style of styles ) {
+      if ( style.parentNode ) {
+        style.parentNode.removeChild( style );
+      }
+    }
+
+    const body = dom.window.document.querySelector( 'body' );
+
+    return res.status( 200 ).json( body?.innerHTML || '' );
+  } catch ( err ) {
+    return res.status( 500 ).json( err );
   }
-
-  const body = dom.window.document.querySelector( 'body' );
-
-  return res.send( body?.innerHTML || '' );
 } );
 
 app.listen( port, () => {
