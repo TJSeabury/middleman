059aaf2adca1dadb6287628678b0a0abb1273947
TJSeabury <tylerseabury@gmail.com>
2022-11-21 13:43:11 -0500

    wrapped with basic logging

--- src/routes/api/basic/+server.ts
+++ src/routes/api/basic/+server.ts
@@ -1,4 +1,8 @@
 import type { RequestHandler } from './$types';
+import withLogger, { _filename } from '$lib/logger';
 import basicHandler from '$lib/routeHandlers/basic';
 
-export const GET: RequestHandler = (routeParams) => basicHandler(routeParams);
+export const GET: RequestHandler = withLogger(
+  (routeParams) => basicHandler(routeParams),
+  _filename(import.meta.url)
+);
