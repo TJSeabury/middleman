eb9c0e727406d1815df6b0ca62f0a135ca8348f4
TJSeabury <tylerseabury@gmail.com>
2022-11-21 15:46:25 -0500

    moved wait to general

--- src/lib/general.ts
+++ src/lib/general.ts
@@ -5,3 +5,5 @@ export const atob = (str: string) => Buffer.from(str, 'base64').toString('utf8')
 export function pipe<T>(V: T, funcs: Array<(x: T) => T>) {
   return funcs.reduce((v, f) => f(v), V);
 }
+
+export const wait = (duration: number) => new Promise((res) => setTimeout(res, duration));
