e1cf2319fa532e056580b2a605d755d52f4a4865
TJSeabury <tylerseabury@gmail.com>
2022-12-01 17:02:58 -0500

    updated rates route to use improved cache

--- src/routes/api/cornerstonebank/rates/+server.ts
+++ src/routes/api/cornerstonebank/rates/+server.ts
@@ -3,4 +3,16 @@ import withLogger, { _filename } from '$lib/logger';
 import cornerstoneBankRatesHandler from '$lib/routeHandlers/cornerstoneBankRates';
 import withCache from '$lib/cacher';
 
-export const GET: RequestHandler = withCache(async (routeParams) => await cornerstoneBankRatesHandler(routeParams));
+const {
+  cachedHandler,
+  invalidator,
+  revalidator
+} = withCache(
+  async (routeParams) => await cornerstoneBankRatesHandler(routeParams),
+  '/api/cornerstonebank/rates'
+);
+
+export const invalidate = invalidator;
+export const revalidate = revalidator;
+
+export const GET: RequestHandler = cachedHandler;
